98ef73d855b82a5bc52b2b1d54fb46ce82616041
Carolina Floro <carolinarochaf@hotmail.com>
2024-07-19 14:49:04 -0300

    .

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 func main() {
 	displayInventory("inventory.csv")
-	setProduct()
-	displayInventory("inventory.csv")
+
+	c := menu()
+
+	if c == "y" {
+		setProduct()
+		displayInventory("inventory.csv")
+		menu()
+	}
+
+	os.Exit(0)
+
+}
+func menu() string {
+	fmt.Println("Do you want to create a new product? y/n ")
+	var c string
+	fmt.Scanln(&c)
+
+	if c != "y" && c != "n" {
+		fmt.Println("You must type y for yes or n for no")
+		menu()
+	}
+	
+	return c
 }
--- product.interface.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type product interface {
-	getId() int
-	getName() string
-	getQuantity() int
-	getPrice() float64
-	getTotalValue() float64
-}
